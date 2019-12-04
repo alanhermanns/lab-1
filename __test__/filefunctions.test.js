0ae23b059f4752ee8ad199c7fa006a71156062b2
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-12-04 14:27:04 -0800

    passes test 1 of writeJSON

--- __test__/filefunctions.test.js
+++ __test__/filefunctions.test.js
@@ -1,14 +1,19 @@
 const { 
-  mkdirp
+  mkdirp,
+  writeJSON
 } = require('../lib/utilityFileFunctions');
 
 const fs = require('fs').promises;
 
 const mockPath = 'Directory Created';
+const mockFilePath = 'File Created';
+const data = { 'this' : 'this' };
 
 jest.mock('fs', () => ({
   promises : {
     mkdir: jest.fn(() => Promise.resolve()),
+    writeFile: jest.fn(() => Promise.resolve())
+
   }
 }));
 
@@ -16,5 +21,16 @@ describe('make a directory and parents', () => {
   it('properly creates a directory', () => {
     return mkdirp(mockPath).then(() =>
       expect(fs.mkdir).toHaveBeenLastCalledWith(mockPath, { recursive : true }));
+  });
+});
+
+describe('write JSON to a file', () => {
+  it('properly creates a path to write JSON to a file', () => {
+    return writeJSON(mockFilePath, data).then(() =>
+      expect(fs.writeFile).toHaveBeenLastCalledWith(mockFilePath, data));
+  });
+  it('verifies that data passed to writeJSON is valid json', () => {
+    return writeJSON(mockFilePath, data).then(() =>
+      expect(fs.writeFile).toThrowErrorMatchingSnapshot());
   });
 });
